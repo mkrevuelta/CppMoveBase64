1523aead5ebc50c60c279ba0fb07b418d070d53e
Martin Knoblauch Revuelta <mkrevuelta@gmail.com>
2019-05-17 01:35:59 +0200

    Support also easy creation of move-only PIMPLs

--- Library/interface/CppMoveBase64/PImplMacros.h
+++ Library/interface/CppMoveBase64/PImplMacros.h
@@ -13,75 +13,106 @@
 #include "ApiMacros.h"
 #include "UniquePtr.h"
 
-#define CMBASE64_DECLARE_PIMPL(CLASS)                     \
-                                                          \
-private:                                                  \
-                                                          \
-    struct Impl;                                          \
-    UniquePtr<Impl> pImpl;                                \
-                                                          \
-public:                                                   \
-                                                          \
-    CLASS () CMBASE64_NOEXCEPT;                           \
-    CLASS (CLASS &&) CMBASE64_NOEXCEPT;                   \
-    CLASS (const CLASS &);                                \
-    ~CLASS () CMBASE64_NOEXCEPT;                          \
-    CLASS & operator= (CLASS &&) CMBASE64_NOEXCEPT;       \
-    CLASS & operator= (const CLASS &);                    \
+// ------------------------------------------------------------ //
+
+#define CMBASE64_DECLARE_MOVE_ONLY_PIMPL(CLASS)                  \
+                                                                 \
+private:                                                         \
+                                                                 \
+    struct Impl;                                                 \
+    UniquePtr<Impl> pImpl;                                       \
+                                                                 \
+public:                                                          \
+                                                                 \
+    CLASS () CMBASE64_NOEXCEPT;                                  \
+    CLASS (CLASS &&) CMBASE64_NOEXCEPT;                          \
+    ~CLASS () CMBASE64_NOEXCEPT;                                 \
+    CLASS & operator= (CLASS &&) CMBASE64_NOEXCEPT;              \
     void swap (CLASS &) CMBASE64_NOEXCEPT;
 
-#define CMBASE64_INLINE_SYMMETRIC_SWAP(CLASS)             \
-                                                          \
-inline void swap (CLASS & a, CLASS & b) CMBASE64_NOEXCEPT \
-{                                                         \
-    a.swap (b);                                           \
+// ------------------------------------------------------------ //
+
+#define CMBASE64_DECLARE_COPYABLE_PIMPL(CLASS)                   \
+                                                                 \
+    CMBASE64_DECLARE_MOVE_ONLY_PIMPL (CLASS)                     \
+                                                                 \
+public:                                                          \
+                                                                 \
+    CLASS (const CLASS &);                                       \
+    CLASS & operator= (const CLASS &);
+
+// ------------------------------------------------------------ //
+
+#define CMBASE64_INLINE_SYMMETRIC_SWAP(CLASS)                    \
+                                                                 \
+inline void swap (CLASS & a, CLASS & b) CMBASE64_NOEXCEPT        \
+{                                                                \
+    a.swap (b);                                                  \
 }
 
-#define CMBASE64_IMPLEMENT_INNER_PIMPL(FULLNAME,CLASS)    \
-                                                          \
-FULLNAME::CLASS () CMBASE64_NOEXCEPT                      \
-{                                                         \
-}                                                         \
-                                                          \
-FULLNAME::CLASS (FULLNAME && other) CMBASE64_NOEXCEPT     \
-    :                                                     \
-    pImpl(std::move(other.pImpl))                         \
-{                                                         \
-}                                                         \
-                                                          \
-FULLNAME::CLASS (const FULLNAME & other)                  \
-    :                                                     \
-    pImpl(other.pImpl ? *other.pImpl.get()                \
-                      : Impl())                           \
-{                                                         \
-}                                                         \
-                                                          \
-FULLNAME::~CLASS () CMBASE64_NOEXCEPT                     \
-{                                                         \
-}                                                         \
-                                                          \
-FULLNAME & FULLNAME::operator= (                          \
-                FULLNAME && other) CMBASE64_NOEXCEPT      \
-{                                                         \
-    pImpl = std::move (other.pImpl);                      \
-    return *this;                                         \
-}                                                         \
-                                                          \
-FULLNAME & FULLNAME::operator= (                          \
-                const FULLNAME & other)                   \
-{                                                         \
-    FULLNAME tmp(other);                                  \
-    swap (tmp);                                           \
-    return *this;                                         \
-}                                                         \
-                                                          \
-void FULLNAME::swap (FULLNAME & other) CMBASE64_NOEXCEPT  \
-{                                                         \
-    pImpl.swap (other.pImpl);                             \
+// ------------------------------------------------------------ //
+
+#define CMBASE64_IMPLEMENT_INNER_MOVE_ONLY_PIMPL(FULLNAME,CLASS) \
+                                                                 \
+FULLNAME::CLASS () CMBASE64_NOEXCEPT                             \
+{                                                                \
+}                                                                \
+                                                                 \
+FULLNAME::CLASS (FULLNAME && other) CMBASE64_NOEXCEPT            \
+    :                                                            \
+    pImpl(std::move(other.pImpl))                                \
+{                                                                \
+}                                                                \
+                                                                 \
+                                                                 \
+FULLNAME::~CLASS () CMBASE64_NOEXCEPT                            \
+{                                                                \
+}                                                                \
+                                                                 \
+FULLNAME & FULLNAME::operator= (                                 \
+                FULLNAME && other) CMBASE64_NOEXCEPT             \
+{                                                                \
+    pImpl = std::move (other.pImpl);                             \
+    return *this;                                                \
+}                                                                \
+                                                                 \
+void FULLNAME::swap (FULLNAME & other) CMBASE64_NOEXCEPT         \
+{                                                                \
+    pImpl.swap (other.pImpl);                                    \
 }
 
-#define CMBASE64_IMPLEMENT_PIMPL(CLASS)                   \
-        CMBASE64_IMPLEMENT_INNER_PIMPL (CLASS, CLASS)
+// ------------------------------------------------------------ //
+
+#define CMBASE64_IMPLEMENT_INNER_COPYABLE_PIMPL(FULLNAME,CLASS)  \
+                                                                 \
+    CMBASE64_IMPLEMENT_INNER_MOVE_ONLY_PIMPL(FULLNAME,CLASS)     \
+                                                                 \
+FULLNAME::CLASS (const FULLNAME & other)                         \
+    :                                                            \
+    pImpl(other.pImpl ? *other.pImpl.get()                       \
+                      : Impl())                                  \
+{                                                                \
+}                                                                \
+                                                                 \
+FULLNAME & FULLNAME::operator= (                                 \
+                const FULLNAME & other)                          \
+{                                                                \
+    FULLNAME tmp(other);                                         \
+    swap (tmp);                                                  \
+    return *this;                                                \
+}
+
+// ------------------------------------------------------------ //
+
+#define CMBASE64_IMPLEMENT_MOVE_ONLY_PIMPL(CLASS)                \
+        CMBASE64_IMPLEMENT_INNER_MOVE_ONLY_PIMPL (CLASS, CLASS)
+
+// ------------------------------------------------------------ //
+
+#define CMBASE64_IMPLEMENT_COPYABLE_PIMPL(CLASS)                 \
+        CMBASE64_IMPLEMENT_INNER_COPYABLE_PIMPL (CLASS, CLASS)
+
+// ------------------------------------------------------------ //
 
 #endif // __CPPMOVEBASE64_PIMPLMACROS_H__
 
