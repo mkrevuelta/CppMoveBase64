90c2059f043fc226c7449b6545a5a5eb3bac1797
Martin Knoblauch Revuelta <mkrevuelta@gmail.com>
2019-05-27 15:12:22 +0200

    Fix the underlying type of the ErrorStatus enum: int8_t

--- Library/interface/CppMoveBase64/ErrorStatus.h
+++ Library/interface/CppMoveBase64/ErrorStatus.h
@@ -11,11 +11,12 @@
 #define __CPPMOVEBASE64_ERRORSTATUS_H__
 
 #include "ApiMacros.h"
+#include <cstdint>
 
 namespace cmbase64
 {
 
-enum class ErrorStatus
+enum class ErrorStatus : int8_t
 {
     NoError, Exception, BadAlloc, DoubleException
 };
